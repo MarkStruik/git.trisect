cb93bdf374f2f9a5a0f2689e373f6b6601df0c39
Mark Struik <mark.struik@devops.ncia.nato.int>
2023-08-11 12:13:32 +0000

    Showing git log messages of current folder

--- main.go
+++ main.go
@@ -4,79 +4,102 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type item struct {
-	title, desc string
-}
-
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
-
 type model struct {
 	list     list.Model
-	selected item
-	width    int
+	selected gitLog
 	height   int
-	log      string
+	width    int
 }
 
 func initialModel() model {
-	items := []list.Item{
-		item{title: "Something", desc: "special"},
-		item{title: "Buy", desc: "me a coffee"},
-	}
+	items := []list.Item{}
 
 	m := model{
 		list: list.New(items, list.NewDefaultDelegate(), 0, 0),
 	}
-	m.list.Title = "What should we buy?"
+	m.list.Title = "What is the first good commit?"
 	return m
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.EnterAltScreen
+	return tea.Batch(tea.EnterAltScreen, getGitLogs)
 }
 
 func getGitLogs() tea.Msg {
-	out, err := exec.Command("git", "log").Output()
-	var message string
+	out, err := exec.Command("git", "log", "--format='%H||%an||%as||%s").Output()
 	if err != nil {
-		message = err.Error()
-		return gitLogMsg(message)
+		return gitLogMsg{error: err}
 	}
-	// todo: build our own model to cope with the logs :)
-	return gitLogMsg(fmt.Sprintf("The git log is:\n%s", out))
+
+	gitLogString := string(out)
+	x := strings.Split(gitLogString, "\n")
+	items := []gitLog{}
+	for _, line := range x {
+		cols := strings.Split(line, "||")
+		fmt.Println(cols)
+		if len(cols) == 4 {
+			items = append(items, gitLog{
+				githash: cols[0],
+				author:  cols[1],
+				date:    cols[2],
+				message: cols[3],
+			})
+		}
+	}
+
+	return gitLogMsg{logs: items, error: nil}
 }
 
-type gitLogMsg string
+type gitLog struct {
+	githash string
+	author  string
+	date    string
+	message string
+}
+
+func (i gitLog) Title() string       { return i.author + " " + i.message }
+func (i gitLog) Description() string { return i.date + " " + i.githash }
+func (i gitLog) FilterValue() string { return i.message }
+func (i gitLog) Item() gitLog        { return i }
+
+type gitLogMsg struct {
+	logs  []gitLog
+	error error
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case gitLogMsg:
-		m.log = string(msg)
+		items := m.list.Items()
+		if msg.error != nil {
+			// todo: error handling
+			return m, nil
+		}
+		for _, item := range msg.logs {
+			items = append(items, item)
+		}
+		m.list.SetItems(items)
 
 	case tea.WindowSizeMsg:
 		m.height = msg.Height
 		m.width = msg.Width
-		m.list.SetSize(m.width, m.height/2)
+		m.list.SetSize(m.width, m.height)
 	// Is it a key press?
 	case tea.KeyMsg:
 
 		// Cool, what was the actual key pressed?
 		switch msg.String() {
-		case "ctrl+l":
-			return m, getGitLogs
-
 		// These keys should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "enter":
-			m.selected = m.list.SelectedItem().(item)
+			m.selected = m.list.SelectedItem().(gitLog)
 			return m, tea.Quit
 		}
 	}
@@ -93,9 +116,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	// The header
 	s := m.list.View()
-	if m.log != "" {
-		s += "\n" + m.log
-	}
+
 	return s
 }
 
@@ -106,7 +127,7 @@ func main() {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
-	if m, ok := m.(model); ok && m.selected.title != "" {
-		fmt.Println(m.selected.title)
+	if m, ok := m.(model); ok && m.selected.message != "" {
+		fmt.Println(m.selected.message)
 	}
 }
